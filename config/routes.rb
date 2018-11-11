1d60315e6b10dad21a8c7d65138cee0be22b49a3
estepollito <esteban.palaciove@gmail.com>
2018-11-11 13:03:20 +1000

    Se realizaron los cambios propuestos en el documento 181105_controlCambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+
+
   get 'main/index'
   get 'main/services'
   get 'main/aboutUs'
+
   root 'main#index'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
