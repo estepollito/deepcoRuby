023b70ed8e44db428352b2f25cfc13a52f6152dc
Esteban <esteban.palaciove@gmail.com>
2019-08-19 16:38:08 -0500

    Acta de actividades del 19/08/19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,12 +92,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
 
+  # FOR MAIL
 
-# FOR MAIL
-
-config.action_mailer.default_url_options = { host: 'https://your app.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://your app.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
@@ -109,3 +107,7 @@ config.action_mailer.default_url_options = { host: 'https://your app.herokuapp.c
   :authentication => :plain,
   :enable_starttls_auto => true
   }
+
+end
+
+
