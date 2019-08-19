6c091702c1e253d0636725805d439f1a7a3e3625
Esteban <esteban.palaciove@gmail.com>
2019-08-19 17:49:36 -0500

    Acta de actividades del 19/08/19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   # FOR MAIL
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'deepco.com.co' }
+  config.action_mailer.default_url_options = { host: 'www.deepco.com.co' }
 
 end
 
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,7 +2,7 @@
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
-  domain: 'yourdomain.com',
+  domain: 'www.deepco.com.co',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   authentication: :login,
