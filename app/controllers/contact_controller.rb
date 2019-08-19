2354c5554f11e3a5b6051c1ff63e4c3ae32e25b6
Esteban <esteban.palaciove@gmail.com>
2019-08-19 18:27:36 -0500

    Acta de actividades del 19/08/19

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -13,9 +13,6 @@ class ContactController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     @contact.request = request
-    if @contact.save
-      ContactMailer.new_request(@contact.id).deliver_later
-    end
     if @contact.deliver
       flash.now[:error] = nil
       redirect_to root_path, notice: 'Message sent successfully'
