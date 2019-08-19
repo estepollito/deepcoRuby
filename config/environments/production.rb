e726aefacc428e4a4d87287fcf4de88fe221cbd1
Esteban <esteban.palaciove@gmail.com>
2019-08-19 17:10:48 -0500

    Acta de actividades del 19/08/19

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['app112519758@heroku.com'],
+  :password => ENV['Esteban1992xd'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
