297cc46cbe23d0308056fec32f5f827d65dea7df
Esteban <esteban.palaciove@gmail.com>
2019-08-19 17:04:06 -0500

    Acta de actividades del 19/08/19

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,4 @@
-class ContactsController < ApplicationController
+class ContactController < ApplicationController
 
   def new 
     @contact = Contact.new
@@ -15,7 +15,7 @@ class ContactsController < ApplicationController
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
-  redirect_to root_path, notice: 'Message sent successfully'
+      redirect_to root_path, notice: 'Message sent successfully'
     else
       flash.now[:error] = 'Cannot send message'
       render :new
