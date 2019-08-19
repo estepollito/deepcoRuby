54455618ba737a2e30fe3400318815a55657b50b
Esteban <esteban.palaciove@gmail.com>
2019-08-19 17:19:15 -0500

    Acta de actividades del 19/08/19

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :contacts, only: [:new, :create]
-
   scope "(:locale)", locale: /en|es/ do
 
   	root 'main#index'
+
+    resources :contacts, only: [:new, :create]
 
   	get 'main/index'
   	get 'main/services'
